c471bf4fe6f0fa8555c7f95e1935c34994989022
MIke <santo.michael@privyid.tech>
2024-02-11 16:12:01 +0700

    Initial commit

---
+++ Movie+iTunes/Modules/Detail/Presentation/DetailVM.swift
@@ -0,0 +1,8 @@
+//
+//  DetailVM.swift
+//  Movie+iTunes
+//
+//  Created by Santo Michael on 11/02/24.
+//
+
+import Foundation
---
+++ Movie+iTunes/Modules/Home/Presentation/HomeVM.swift
@@ -0,0 +1,8 @@
+//
+//  HomeVM.swift
+//  Movie+iTunes
+//
+//  Created by Santo Michael on 11/02/24.
+//
+
+import Foundation

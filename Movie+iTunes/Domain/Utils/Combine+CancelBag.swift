89adbf41f40ed8d48f32a372649eca2713c874f0
MIke <santo.michael@privyid.tech>
2024-02-17 03:20:47 +0700

    add internal doc on CancelBag

--- Movie+iTunes/Domain/Utils/Combine+CancelBag.swift
+++ Movie+iTunes/Domain/Utils/Combine+CancelBag.swift
@@ -7,19 +7,27 @@
 
 import Combine
 
+/// A class for managing cancellable subscriptions.
 public class CancelBag {
+	/// A set containing the cancellable subscriptions.
 	public var subscriptions = Set<AnyCancellable>()
 	
+	/// Cancels all subscriptions and removes them from the bag.
 	public func cancel() {
 		subscriptions.forEach { $0.cancel() }
 		subscriptions.removeAll()
 	}
 	
+	/// Initializes a new instance of `CancelBag`.
 	public init() {}
 }
 
 extension AnyCancellable {
+	/// Stores the subscription in the specified `CancelBag`.
+	///
+	/// - Parameter cancelBag: The `CancelBag` to store the subscription in.
 	public func store(in cancelBag: CancelBag) {
 		cancelBag.subscriptions.insert(self)
 	}
 }
+

c60f30d29ead3d19dfc4c5bc71d49b44243ccdbe
MIke <santo.michael@privyid.tech>
2024-02-17 03:20:17 +0700

    add internal doc on Atlas module

--- Movie+iTunes/Domain/Atlas/Atlas.swift
+++ Movie+iTunes/Domain/Atlas/Atlas.swift
@@ -5,9 +5,14 @@
 //  Created by Santo Michael on 13/02/24.
 //
 
+/// An internal class responsible for routing within the Atlas framework.
 internal class Atlas {
+	/// A static router closure used for routing within the framework.
 	internal static var router: ((Route) -> Void)?
 	
+	/// Routes to the specified destination.
+	///
+	/// - Parameter to: The destination to route to.
 	internal static func route(to: Route) {
 		if let router = Atlas.router {
 			router(to)
@@ -15,11 +20,16 @@ internal class Atlas {
 	}
 }
 
+/// An enumeration representing various routes within the Atlas framework.
 internal enum Route {
+	/// Represents a detail route, typically for displaying movie details.
 	case detail(movie: Movie, favoriteCompletion: ((Movie) -> Void)?)
+	/// Represents a component route, used for displaying various UI components.
 	case component(Component)
 	
+	/// An enumeration representing different types of UI components.
 	enum Component {
+		/// Represents a toast component.
 		case toast(image: String, title: String, description: String)
 	}
 }
